7fd6194375d40ee31d1f8ebf0c99142c1952a622
Satyajeet Das <99283330+satyajeet001@users.noreply.github.com>
2024-05-14 21:58:57 +0530

    Update Connect.jsx

--- portfolio-website/src/assets/Connect.jsx
+++ portfolio-website/src/assets/Connect.jsx
@@ -30,7 +30,7 @@ function ConnectForm() {
     e.preventDefault();
     console.log(formData);
     try {
-      const response = await fetch(`https://portfolio-api-kappa-two.vercel.app`, {
+      const response = await fetch(`https://portfolio-api-kappa-two.vercel.app/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
