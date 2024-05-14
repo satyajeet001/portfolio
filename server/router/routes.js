f2b9bd3c08e98a438403e69d297840e2d525c869
Satyajeet Das <99283330+satyajeet001@users.noreply.github.com>
2024-05-14 19:03:58 +0530

    Update routes.js

--- server/router/routes.js
+++ server/router/routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/controllers");
 
-router.route("/contact").post(controllers.contactForm);
+router.route("/").post(controllers.contactForm);
 
 module.exports = router;
