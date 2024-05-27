c8cd74a38c19cf69edbe14016326505beccc5b7b
Satyajeet Das <99283330+satyajeet001@users.noreply.github.com>
2024-05-27 21:55:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ const app = express();
 
 const corsOptions = {
   origin: [
-   
     "https://satyajeet-das.vercel.app",
-    
+   "https://satyajeet-das.vercel.app/#home",
+    "https://satyajeet-das.vercel.app/#skills",
+    "https://satyajeet-das.vercel.app/#projects",  
   ],
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
